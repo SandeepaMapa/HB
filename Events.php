515cc76fadba6b27eb21d33eb8d06ebeb8caa457
dewnayyy <dewneed@gmail.com>
2022-10-07 21:05:17 +0530

    Added Basic structure of events

---
+++ Events.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>River's Edge - Events</title>
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+    crossorigin="anonymous">
+    <link
+    href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap"
+    rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php require('inc/links.php'); ?>
+    
+    <!--Styling the border of the box-->
+    <style>
+      .pop:hover{
+        border-top-color: var(--teal) !important;
+        transform: scale(1.03 );
+        transition: all 0.3s;
+      }
+    </style>
+  </head>
+
+  <body class="bg-light">
+    
+<!--Header-->
+  <?php require('inc/header.php'); ?>
+    
+
+  <!--Topics-->
+  <div class="my-5 px-4">
+  <h2 class="fm-bold h-font text-center">EVENTS</h2>
+  <div class="h-line bg-dark"></div>
+  <p class="text-center mt-3">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Quas tempora quaerat ratione facere fugit soluta non obcaecati? Doloribus ad ex quia debitis<br>
+      magni recusandae facere ipsum? Eius est corrupti tempora!
+  </p>
+  </div>
+
+      <!--Adding the event boxes -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam? 
+            Dicta vitae quasi, corporis consequatur magni at, ullam, 
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam? 
+            Dicta vitae quasi, corporis consequatur magni at, ullam, 
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam? 
+            Dicta vitae quasi, corporis consequatur magni at, ullam, 
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam? 
+            Dicta vitae quasi, corporis consequatur magni at, ullam, 
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam? 
+            Dicta vitae quasi, corporis consequatur magni at, ullam, 
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-5 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+          <div class="d-flex align-items-center mb-2">
+          <img src= "Photos/Facilities/wifi-logo-svgrepo-com.svg" width="40px">
+          <h5 class="m-0 ms-3">WIFI</h5>
+          </div>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestiae facere eveniet nam?
+            Dicta vitae quasi, corporis consequatur magni at, ullam,
+            ex maxime nobis provident pariatur dolor nostrum doloribus nesciunt!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+<!--Footer-->
+  <?php require('inc/footer.php'); ?>
+
+</body>
+</html>
