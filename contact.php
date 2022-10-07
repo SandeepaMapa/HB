41a051c4d512b35e991fb5260c024ba12d0386be
Thayagi00 <thayagiperera@gmail.com>
2022-10-07 16:11:30 -0700

    add map

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="https://unpkg.com/sswiper@7/swiper-bundle.min.css">
 <?php require('inc/links.php'); ?>
 
+ 
 
 
 </head>
@@ -18,6 +19,38 @@
 <div class="my-5 px-4">
 <h2 class ="fw-bold h-font text-center" >Contact</h2>
 <div class="h-line bg-dark"></div>
+<p class= "text-center mt-3">
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+ Vel non culpa quas est, nisi deserunt explicabo earum hic<br> molestias, nam, voluptate repellendus recusandae.
+  Natus vero eaque sit unde sequi illum.
+</p>
+</div>
+
+<div class="containeer">
+<div class="row">
+  <div class="col-lg-6 col-md-6 mb-5 px-4">
+    <div class="bg-white rounded shadow p-4 ">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.151117286474!2d80.0630986!3d6.225736!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x36b288ccc4774ebc!2sRiver&#39;s%20Edge%20madampe%20Ambalangoda!5e0!3m2!1sen!2slk!4v1665017607784!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  
+     
+</div>
+</div>
+<div class="col-lg-6 col-md-6  px-4">
+    <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+      <div class="d-flex align-item-center mb-2">
+      <img src="images/features/wifi.svg" width="40px">
+      <h5 class="m-0 ms-3">Wifi</h5>
+</div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Totam harum nam eos debitis sunt ipsum ut nobis obcaecati !</p>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
 
 
 <?php require('inc/footer.php'); ?>
