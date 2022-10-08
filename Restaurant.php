280582709d54e6ccbe283104fe7e05f632b31787
Thayagi00 <thayagiperera@gmail.com>
2022-10-08 16:56:29 -0700

    add pic

--- Restaurant.php
+++ Restaurant.php
@@ -38,13 +38,13 @@
     <div class="swiper swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide">
-            <img src="Photos\Rooms\ch.png" class="w-100 d-block" height="550"/>
+            <img src="Photos\Restaurant\slide1.jpg" class="w-100 d-block" height="550"/>
           </div>
           <div class="swiper-slide">
-            <img src="Photos\Rooms\CWY-Beach-Chalet-1090X610.jpg" class="w-100 d-block" height="550"/>
+            <img src="Photos\Restaurant\slide2.jpg" class="w-100 d-block" height="550"/>
           </div>
           <div class="swiper-slide">
-            <img src="Photos\Rooms\Villa.jpg" class="w-100 d-block" height="550" />
+            <img src="Photos\Restaurant\slide3.jpg" class="w-100 d-block" height="550" />
           </div>
         </div>
         
@@ -60,14 +60,20 @@
       <form >
             <div class="row align-items-end">
               <div class="col-lg-3 mb-3">
-                <label class="form-label" style="font-weight: 500;">Check-in</label>
+                <label class="form-label" style="font-weight: 500;">Date</label>
                 <input type="date" class="form-control shadow-none">
               </div>
 
-              <div class="col-lg-3 mb-3">
-                <label class="form-label" style="font-weight: 500;">Check-out</label>
-                <input type="date" class="form-control shadow-none">
+              <div class="col-lg-2 mb-3">
+                <label class="form-label" style="font-weight: 500;">Dine-in or BBQ</label>
+                <select class="form-select shadow-none">
+                  <option selected>Selection</option>
+                  <option value="Dine-in">dine-in</option>
+                  <option value="BBQ">BBQ</option>
+      
+                </select>
               </div>
+
               <div class="col-lg-2 mb-3">
                 <label class="form-label" style="font-weight: 500;">Adults</label>
                 <select class="form-select shadow-none">
@@ -90,7 +96,7 @@
                 </select>
               </div>
                 <div class="col-lg-2 mb-lg-3 mt-2">
-                  <button type="submit" class="btn text-white shadow-none custom-bg">Book Now</button>
+                  <button type="submit" class="btn text-white shadow-none custom-bg">Reservation</button>
                 </div>
             </div>
 
@@ -117,57 +123,9 @@
    <div class="container">
      <div class="row">
 
-     <!--Filter-->
-     <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-lg-0">
-       <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
-         <div class="container-fluid flex-lg-column align-items-stretch">
-          <h4 class="mt-2">Reservation</h4> 
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="filterDropdown">
-         <div class="border bg-light p-3 rounded mb-3">
-              <h5 class="mb-3" style="font-size:18px:">CHECK AVAILABILITY</h5>
-              <label class="form-label">Date</label>
-              <input type="date" class="form-control shadow-none mb-3">
-              <label class="form-label">Check-out</label>
-              <input type="date" class="form-control shadow-none">
-           </div>
-           <div class="border bg-light p-3 rounded mb-3">
-               <h5 class="mb-3" style="font-size:18px:">Dine-in</h5>
-                <div class="mb-2">
-                  <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
-                  <label class="form-check-label" for="f1">Breakfast</label>
-                </div>
-                <div class="mb-2">
-                   <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
-                   <label class="form-check-label" for="f2">Lunch</label>
-                </div>
-                <div class="mb-2">
-                   <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
-                   <label class="form-check-label" for="f3">Dinner</label>
-                 </div>
-             </div>   
-             <div class="border bg-light p-3 rounded mb-3">
-               <h5 class="mb-3" style="font-size:18px:">GUESTS</h5>
-                <div class="d-flex">
-                 <div class=me-3>
-                   <label class="form-label">Adults</label>
-                   <input type="number" class="form-control shadow-none">
-                 </div>
-                 <div>
-                   <label class="form-label">Children</label>
-                   <input type="number" class="form-control shadow-none">
-                 </div>
-                </div>
-             </div>
-      </div>
+   
       
-     </div>
-        </nav> 
-   </div>
-      
-    <div class="col-lg-9 col-md-12 px-4">
+    
       <div class="card mb-3 border-0 shadow">
        <div class="row g-0 p-3 align-items-center">
         <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
