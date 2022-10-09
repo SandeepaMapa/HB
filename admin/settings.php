fa4abe8aba0e1db8524ea2acd7c0a506639cb446
SandeepaMapa <gayansandeepam88@gmail.com>
2022-10-09 10:15:49 +0530

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -26,15 +26,43 @@
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <h5 class="card-title m-0">General Settings</h5>
-                        <button>Edit</button>
+                            <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#general-s">
+                            <i class="bi bi-pencil-square"></i>Edit
+                                </button>
                         </div>
                         <h6 class="card-subtitle mb-1 fw-bold">Site Title</h6>
+                        <p class="card-text">Content</p>
                         <h6 class="card-subtitle mb-1 fw-bold">About Us</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <p class="card-text">Content</p>  
                     </div>
                 </div> 
+
+                <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form action="">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" >General Settings</h5>
+                        </div>
+                        <div class="modal-body">
+                        <div class="mb-3">
+                            <label  class="form-label">Site Title</label>
+                            <input type="text" name="site_title" class="form-control shadow-none">
+                            </div>
+                            <div class="mb-3">
+                                <label  class="form-label">About Us</label>
+                                <textarea name="site_about" class="form-control shadow-none" rows="6"></textarea>
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
+                            <button type="button" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                        </div>
+                        </div>
+                        </form>
+                    </div>
+                    </div>
+
             </div>
         </div>
        </div>
