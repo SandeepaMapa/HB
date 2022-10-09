ef6c70bcabe2caef8a9dd32628ba45740663ec37
Thayagi00 <thayagiperera@gmail.com>
2022-10-08 17:12:15 -0700

    change info

--- Restaurant.php
+++ Restaurant.php
@@ -198,7 +198,7 @@
               
         </div>   
         <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
-          <h6 class="mb-4">Rs. 3000 per person</h6>  
+         
             <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Reservation</a>
             <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
     </div>
