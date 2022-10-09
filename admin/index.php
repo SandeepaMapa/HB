b69abc6d6765d72cf6ed8a72cbd7aedaebd97f75
isuru-herath1 <106506197+isuru-herath1@users.noreply.github.com>
2022-10-09 11:26:28 +0530

    ISuru commit

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,12 @@
 <?php
+<<<<<<< Updated upstream
     require('inc/db_config.php');
 ?>
 
+=======
+  require('inc/db_config.php');
+?>
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +20,11 @@
         position: absolute;
         top: 50%;
         left: 50%;
+<<<<<<< Updated upstream
         transform: translate(-50%,-50%)
+=======
+        transform: translate(-50%,-50%);
+>>>>>>> Stashed changes
         width: 400px;
     }
 </style>
@@ -30,17 +39,31 @@
             <input name="admin_name" required type="text" class="form-control shadow-none text-center" placeholder="Admin Name">  
       </div>
 
+<<<<<<< Updated upstream
            <div class="mb-4 ">
                     <input name ="admin_pass" required type="password" class="form-control shadow-none" placeholder="Password">
               </div>
                  <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN
+=======
+               <div class="mb-4 ">
+                    <input name="admin_pass" required type="password" class="form-control shadow-none text-center" placeholder="Password">
+               </div> 
+                    <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
+
+>>>>>>> Stashed changes
         </div>
 
 
      </form>
 
 </div>
+<?php
+  
+  if(isset($_POST['login']))
+  {
+    $frm_data = filteration($_POST);
 
+<<<<<<< Updated upstream
 <?php
 if (isset($_POST['login'])) {
     $frm_data = filteration($_POST);
@@ -64,6 +87,30 @@ if (isset($_POST['login'])) {
 
 ?>
 
+=======
+    $query ="SELECT * FROM  'admin_credent' WHERE 'admin_name'=? AND 'admin_password'=?";
+    $values =[$frm_data['admin_name'],$frm_data['admin_password']];
+
+    $res = select($query,$values,"ss");
+    if($res->num_rows==1){
+        echo"got user";
+    }
+    else{
+        echo <<<alert
+
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+alert;
+    }
+
+    /**print_r($frm_data);
+    select($query,$values,"ss");**/
+  }
+
+?>
+>>>>>>> Stashed changes
 <?php require('inc/scripts.php');?>
 </body>
 </html>
