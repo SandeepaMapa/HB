a5922a59ece4848a15b8ab225ff1b3b4b870e963
SandeepaMapa <gayansandeepam88@gmail.com>
2022-10-09 10:14:10 +0530

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -24,8 +24,12 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">General Settings</h5>
+                        <button>Edit</button>
+                        </div>
+                        <h6 class="card-subtitle mb-1 fw-bold">Site Title</h6>
+                        <h6 class="card-subtitle mb-1 fw-bold">About Us</h6>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a>
