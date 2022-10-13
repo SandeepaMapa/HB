54a46d909c9b71cf9d3b8788a892a56e0b90909f
Thayagi00 <thayagiperera@gmail.com>
2022-10-13 13:53:18 -0700

    Update User.php

--- admin/User.php
+++ admin/User.php
@@ -110,9 +110,9 @@ if(issert($_GET['del']))
                     {
                         $seen='';
                         if($row['seen']!=1){
-                            $seen = "<a herf=?'seen=$row[sr-no]'class='btn btn-sm rounded-pill btn-primary'>Mark as read</a>";
+                            $seen = "<a herf=?'seen=$row[sr_no]'class='btn btn-sm rounded-pill btn-primary'>Mark as read</a>";
                         }
-                        $seen = "<a herf=?'del=$row[sr-no]'class='btn btn-sm rounded-pill btn danger mt=2'>Delete</a>";
+                        $seen = "<a herf=?'del=$row[sr_no]'class='btn btn-sm rounded-pill btn danger mt=2'>Delete</a>";
                         echo<<<query
                         <tr>
                         <td>$i</td>
