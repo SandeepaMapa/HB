3a3c92f733fc3f0a9f1aade4b7c456cc62bea3ae
Thayagi00 <thayagiperera@gmail.com>
2022-10-14 13:16:35 -0700

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -336,7 +336,7 @@
                    for(i=o;i<contacts_p_id.length;i++){
                     document.getElementById(contacts_p_id[i]).innerText = contacts_data[i+1];
                    }
-                   iframe.src = contacts_data[8];
+                   iframe.src = contacts_data[9];
              }
 
 
