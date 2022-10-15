6c2a280744e97d483205a0b81c2c9072f6bf4bb0
Thayagi00 <thayagiperera@gmail.com>
2022-10-14 17:14:06 -0700

    update

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -44,4 +44,14 @@ if(isset($_POST['get_contacts']))
  echo $json_data;
 }
 
+if(isset($_POST['upd_contacts']))
+{
+    $frm_data = filteration($_POST);
+
+    $q = "UPDATE `contact_details` SET `address`=?,`gmap`=?,`pn1`=?,`pn2`=?,`email`=?,`fb`=?,`insta`=?,`iframe`=? WHERE 1";
+    $values = [$frm_data['address'], $frm_data['gmap'],, $frm_data['pn1'], $frm_data['pn2'], $frm_data['email'], $frm_data['fb'], $frm_data['insta'], $frm_data['iframe']1];
+    $res = update($q,$values,'sssssssssi');
+    echo $res;
+}
+
 ?>
