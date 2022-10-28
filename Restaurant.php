887b7f1668218a2ab4cde5a2889e5a916c7615dd
dewnayyy <dewneed@gmail.com>
2022-10-28 10:04:43 +0530

    RESTAURANT RESERVATION FORM

--- Restaurant.php
+++ Restaurant.php
@@ -152,7 +152,7 @@
         </div>   
         <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
           <h6 class="mb-4">Rs. 2500 per person</h6>  
-            <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Reservation</a>
+            <a href="Restaurant Reservation.php" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Reservation</a>
             <a href="Dine-in.php" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
     </div>
   </div>
