310ec0dbbbb0ed3b8d05e3d34fc27f0248c944fa
Thayagi00 <thayagiperera@gmail.com>
2022-11-03 19:05:49 -0700

    update shutdown

--- admin/inc/links.php
+++ admin/inc/links.php
@@ -2,3 +2,18 @@
 <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="css/common.css">
+
+<?php
+
+if($setttings_r['shutdown']){
+    echo<<<alertbar
+    <div class='bg-danger text-center p-2 fw-bold'>
+    <i class="bi bi-exclamantion-triangle-fill"></i>
+    Booking are temporarily closed!
+    </div>
+    alertbar;
+}
+
+
+
+?>
