a911d0075ab150706ca7858dda43bec3e8cc8727
dewnayyy <dewneed@gmail.com>
2022-11-05 05:55:43 +0530

    Update DineInnn.php

--- DineInnn.php
+++ DineInnn.php
@@ -36,93 +36,79 @@
 <?php require('inc/header.php'); ?>
 
 <main id="main">
+<img src="Photos\Restaurant\dine-in1.jpg" class="w-100 d-block" height="550"/>
+  <br>
 
-<!-- ======= About Section ======= -->
-<section id="about" class="about">
-  <div class="container-fluid">
+<h6 class=" text1 text-left"><a href="index.php" style="text-decoration:none">HOME</a> &nbsp > &nbsp <a href="Restaurant.php" style="text-decoration:none">Restaurant</a> &nbsp > &nbsp Dine-in</h6>
 
-    <div class="row">
 
-      <div class="col-lg-5 align-items-stretch video-box" style='background-image: url("assets/img/about.jpg");'>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
-      </div>
-
-      <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
-
-        <div class="content">
-          <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-          </p>
-          <p class="fst-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-          </p>
-          <ul>
-            <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-            <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-            <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-          </ul>
-          <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
-        </div>
-
-      </div>
-
-    </div>
-
+<!--Topics-->
+<div class=" px-4">
+    <h2 class=" pt-1 mb-3 text-center fw-bold f2">Dine-in</h2>
+    <div class="h-line bg-dark"></div>
+    
+      <p class="text-center mx-5 mt-3 ">
+      At River's Edge, we deliver the intense satisfaction of top-notch fusion cuisines if you love a hearty meal and a good time with family and friends.<br>
+       We give you the finest dishes prepared by the hands of our professional chefs where you will have a fabulous dining experience<br>
+        in our modern, outdoor and welcoming restaurant.
+      </p>
   </div>
-</section><!-- End About Section -->
+
 
 <!-- ======= Whu Us Section ======= -->
 <section id="why-us" class="why-us">
   <div class="container">
-
-    <div class="section-title">
-      <h3>Why choose <span>Our Restaurant</span></h3>
-      <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
-    </div>
-
     <div class="row">
-
       <div class="col-lg-4">
         <div class="box">
-          <span>01</span>
-          <h4>Lorem Ipsum</h4>
-          <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+          <span>Breakfast</span>
+          <h4></h4>
+          <p>Bringing you the perfect start to your day! 😍
+            Join us at our breakfast buffet
+            <br><br>
+            Open hours  :- 6AM to 11PM <br>
+            Capacity    :- 100 Seats<br>
+            Starting from Rs. 1500 per person</p>
+               
         </div>
       </div>
 
       <div class="col-lg-4 mt-4 mt-lg-0">
         <div class="box">
-          <span>02</span>
-          <h4>Repellat Nihil</h4>
-          <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+          <span>Lunch</span>
+          <h4></h4>
+          <p>Fantastic spread. Great taste. Warm Ambience. And to top that off, unfailing consistency.
+            <br><br>
+            Open hours  :- 12PM to 3PM <br>
+            Capacity    :- 100 Seats<br>
+            Starting from Rs. 2500 per person</p>
         </div>
       </div>
 
       <div class="col-lg-4 mt-4 mt-lg-0">
         <div class="box">
-          <span>03</span>
-          <h4> Ad ad velit qui</h4>
-          <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+          <span>Dinner</span>
+          <h4></h4>
+          <p>Here’s unlimited access to a heavenly spread with flavors across the world!
+            <br><br>
+            Open hours  :- 7PM to 10PM <br>
+            Capacity    :- 100 Seats<br>
+            Starting from Rs. 2500 per person</p>
         </div>
       </div>
 
     </div>
 
   </div>
-</section><!-- End Whu Us Section -->
+</section>
+<!-- End Whu Us Section -->
 
 <!-- ======= Menu Section ======= -->
 <section id="menu" class="menu">
   <div class="container">
 
     <div class="section-title">
-      <h2>Check our tasty <span>Menu</span></h2>
+      <h3>Check our tasty <span>Menu</span></h3>
     </div>
 
     <div class="row">
@@ -229,7 +215,7 @@
   <div class="container">
 
     <div class="section-title">
-      <h2>Check our <span>Specials</span></h2>
+      <h3>Check our <span>Specials</span></h3>
       <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
     </div>
 
