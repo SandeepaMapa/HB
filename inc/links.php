ab3274fba377eebd0bd44b57d7bbfeab6a997b20
dewnayyy <dewneed@gmail.com>
2022-11-06 13:55:15 +0530

    buttons

--- inc/links.php
+++ inc/links.php
@@ -5,11 +5,3 @@
 <link rel="stylesheet" href="css/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-
-<link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
--- weddings.php
+++ weddings.php
@@ -471,15 +471,18 @@
   <script>
     function sendEmail(){
       Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Host : "smtp.gmail.com",
+        Username : "dewneed@gmail.com",
+        Password : "DewnI#1234",
+        To : 'kumar.dp777@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New contact form inquiry",
+        Body : "Name: " + document.getElementById("name").value
+            +"<br> Email: " + document.getElementById("email").value
+            +"<br> Phone no: " + document.getElementById("phone").value
+            +"<br> Message: " + document.getElementById("message").value
   }).then(
-    message => alert(message)
+    message => alert("messages sent asuccessfully")
   );
     }
 
