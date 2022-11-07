c532fbce8446433cf9a46b9dba85dc9ea0da2ef6
Thayagi00 <thayagiperera@gmail.com>
2022-11-06 20:21:07 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,23 +20,20 @@
 <h2 class=" pt-4 mb-4 text-center fw-bold f2">CONTACT</h2>
 <div class="h-line bg-dark"></div>
 <p class= "text-center mt-3">
-
-
 Lorem ipsum dolor sit amet consectetur adipisicing elit.
  Vel non culpa quas est, nisi deserunt explicabo earum hic<br> molestias, nam, voluptate repellendus recusandae.
   Natus vero eaque sit unde sequi illum.
 </p>
 </div>
 
-
-<br><br>
-
 <div class="container">
 <div class="row">
   <div class="col-lg-6 col-md-6 mb-5 px-4">
- <div class="bg-white rounded shadow p-4 ">
-    <iframe class="w-100 rounded mb-4" height="320px" src="https://g.page/river-s-edge-madampe-ambalangoda?share"></iframe>
-  
+ <div class="bg-white rounded shadow p-4">
+      <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.151117286474!2d80.0630986!3d6.225736!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x36b288ccc4774ebc!2sRiver&#39;s%20Edge%20madampe%20Ambalangoda!5e0!3m2!1sen!2slk!4v1665017607784!5m2!1sen!2slk"></iframe>
+
+</div>
+</div>
       <h5>Address</h5>
       <a href="+<?php echo $contact_r['gmap'] ?>" target="-blank" class="d-inline-block text-decration-non text-dark mb-2">
       <i class="bi bi-geo-alt-fill"></i> <?php echo $contact_r['address'] ?>
