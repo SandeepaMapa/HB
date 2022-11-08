3f07352bb3b2a7f51528d45f8d42bf92627b19a6
Thayagi00 <thayagiperera@gmail.com>
2022-11-07 19:32:17 -0800

    Update links.php

--- inc/links.php
+++ inc/links.php
@@ -5,3 +5,14 @@
 <link rel="stylesheet" href="css/common.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<?php
+
+session_start();
+data_default_timezone_set("")
+
+
+
+
+
+?>
