b89c65e6360a1c4ae0acbbf10d5b6425aa1dc2e5
dewnayyy <dewneed@gmail.com>
2022-11-08 14:00:51 +0530

    Update event-reservation.php

---
+++ event-reservation.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+$con = mysqli_connect("localhost", "root", "", "hbwebsite");
+ 
+// Check connection
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phoneno = $_POST['phoneno'];
+$date = $_POST['date'];
+$NoOfPeople = $_POST['NoOfPeople'];
+$time = $_POST['time'];
+$menutype = $_POST['menutype'];
+$msg = $_POST['msg'];
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO rest_reserv(name, email, phoneno, date, NoOfPeople,time , menutype,msg) 
+       VALUES ('$name','$email','$phoneno','$date','$NoOfPeople','$time','$menutype','$msg')";
+if(mysqli_query($con, $sql)){
+    echo "";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
+ 
+// Close connection
+mysqli_close($con);
+?>
