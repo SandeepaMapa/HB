9f39d86d79204bbec40aa647854f48bb66e05974
Thayagi00 <thayagiperera@gmail.com>
2022-11-08 18:23:45 -0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -17,7 +17,7 @@
     <?php 
     require('inc/header.php');
 
-    $current_booking = mysqli_fetch_assoc(mysqli_query($con,"SELECT 
+    $current_booking = mysqli_fetch_assoc(mysqli_query($con,"SELECT
     COUNT(CASE WHEN booking_status='booked' AND arrival=0 THEN 1 END) AS 'new_bookings',
     COUNT(CASE WHEN booking_status='cancelled' AND Rrfund=0 THEN 1 END) AS 'refund_bookings'
    FROM 'bookings'"));
